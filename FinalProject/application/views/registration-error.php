f983e633a6c3473c45151f82fef3a31aa1822d20
nadiaborisova <me4o_turist@abv.bg>
2016-01-20 10:27:45 +0200

    dlt

--- FinalProject/application/views/registration-error.php
+++
@@ -1,5 +0,0 @@
-<h3 class="text-center" style="color: white;">ГРЕШКА!!!</h3>
-<br/>
-<h4 class="text-center text-danger"> Вече съществува потребител с <strong> потребителско име <?php echo $username; ?> </strong> и/или <strong>име на фирма <?php echo $companyName; ?> </strong> ! </h4>
-<br/>
-<div><a href="<?php echo base_url();?>agentregform">&lt;&lt; назад към Формата за регистрация </a></div>
--- FinalProject/application/views/update-success.php
+++
@@ -1,6 +0,0 @@
-<h3 class="text-center" style="color: white;">АКТУАЛИЗАЦИЯ</h3>
-<br/>
-<strong><h4 class="text-center text-success"> Данните са актуализирани успешно!</h4></strong>
-<br/>
-<div><a href="<?php echo base_url();?>manageprofile">&lt;&lt; назад към Моят профил</a></div>
-<div><a href="<?php echo base_url();?>manageoffers/display">&lt;&lt; назад към Моите оферти</a></div>
